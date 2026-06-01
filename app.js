eff7cb4b0a78e5b30e04d06b95ca858d066e84b4
plamen-del plamenov-del <plamen-del@plamen.del>
2026-06-01 12:49:06 +0300

    add-to-bbbb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     <!DOCTYPE html>
     <html>
     <head>
-      <title>bbbbbbbbbbbbb</title>
+      <title>test-bbbb</title>
       <style>
         body {
           font-family: Arial, sans-serif;
@@ -45,8 +45,8 @@ const server = http.createServer((req, res) => {
     </head>
     <body>
       <div class="container">
-        <h1>bbbbbbbbbbbbb</h1>
-        <div class="info">bbbbbbbb</div>
+        <h1>testbbbbbbb</h1>
+        <div class="info">testbbbbbbbb</div>
         <div class="pod-info">
           <strong>Pod:</strong> ${process.env.HOSTNAME || 'unknown'}<br>
           <strong>Node:</strong> ${process.env.NODE_NAME || 'unknown'}<br>
