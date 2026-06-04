8ac7cc85cb2673c3f3068d7f73d2e994d2d7317d
plamen-del plamenov-del <plamen-del@plamen.del>
2026-06-04 09:42:48 +0300

    new

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     <!DOCTYPE html>
     <html>
     <head>
-      <title>new-branch-test-bbbb</title>
+      <title>new</title>
       <style>
         body {
           font-family: Arial, sans-serif;
@@ -45,8 +45,8 @@ const server = http.createServer((req, res) => {
     </head>
     <body>
       <div class="container">
-        <h1>new-branch-testbbbbbbb</h1>
-        <div class="info">new-brach-testbbbbbbbb</div>
+        <h1>new</h1>
+        <div class="info">new</div>
         <div class="pod-info">
           <strong>Pod:</strong> ${process.env.HOSTNAME || 'unknown'}<br>
           <strong>Node:</strong> ${process.env.NODE_NAME || 'unknown'}<br>
